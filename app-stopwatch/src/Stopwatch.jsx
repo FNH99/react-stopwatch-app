deb3a9c65fadb3c4610c8d90a4123994ddab388a
FNH99 <148479741+FNH99@users.noreply.github.com>
2024-07-25 00:53:57 +0700

    set const, variables, and functions

--- app-stopwatch/src/Stopwatch.jsx
+++ app-stopwatch/src/Stopwatch.jsx
@@ -2,6 +2,32 @@ import React, {useState, useEffect, useRef} from 'react';
 
 function Stopwatch(){
 
+    const [isRunning, setIsRunning] = useState(false);
+    const [elapsedTime, setElapsedTime] = useState(0);
+    const intervalIdRef = useRef(null);
+    const startTimeRef = useRef(0);
+
+    useEffect(() => {
+
+    }, [isRunning]);
+
+    function start(){
+        
+    }
+    
+    function stop(){
+
+    }
+
+    function reset(){
+
+    }
+
+    function formatTime(){
+
+    }
+
+
     return(<></>);
 }
 
